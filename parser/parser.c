c0e73835a6e6c9569b6503e9a88e3b033d335c36
PAULO HENRIQUE LACERDA DE AMORIM <newbyedeveloper@cock.li>
2018-03-28 08:59:09 -0300

    parsefix1

--- parser/parser.c
+++ parser/parser.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "../lexer/lex.h"
 
-/*    
+/*
 	expr ::= term rest
 	rest ::= + expr | - expr | &
-	term ::= NUM_OR_ID 
+	term ::= NUM_OR_ID
 */
 
 void expr(void);
@@ -13,32 +13,26 @@ void term(void);
 
 void expr(void)
 {
-  while (!match(EOI))
-  {
-	term();
-    rest();
-  }
+  term();
+  rest();
 }
 
 void rest(void)
 {
-  if (match(PLUS) || match(MINUS))
+  if (match(EOI))
+    return;
+  else if (match(PLUS) || match(MINUS))
   {
-	advance();
-    /*if (!match(NUM_OR_ID))
-      puts("Expected number or identifier");
-    else
-      printf("%2.*s", yytext, yyleng);
-    */
-	expr();
-  }  
+  	advance();
+  	expr();
+  }
 }
 
 void term(void)
 {
   if (match(NUM_OR_ID))
   {
-	advance();
+    advance();
   }
   else
     puts("Expected number or identifier");
